167c8626b386babfecfcf53223b7141114c41719
Hassan Khan <40873451+khanmhassan@users.noreply.github.com>
2019-06-01 18:43:24 -0400

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    text = 'default starting text';
+
+
+    constructor() {
+    }
+
+    onChangeText() {
+        this.text = 'Changed!';
+    }
 
 }
